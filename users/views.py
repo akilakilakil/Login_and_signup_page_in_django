547e31ce3c8ff5e32a8e2cc6db22730cd9fcd564
akilakilakil <accesserrorweb@gmail.com>
2018-01-20 20:56:12 +0530

    first commit

---
+++ users/views.py
@@ -0,0 +1,24 @@
+from django.contrib.auth import logout
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.urlresolvers import reverse,reverse_lazy
+from django.http import HttpResponseRedirect 
+from django.shortcuts import render
+from django.views.generic import FormView,CreateView
+
+from . import forms
+# Create your views here.  
+def dashboard(request):
+    return render(request, 'users/dashboard.html')
+
+class LogoutView(LoginRequiredMixin,FormView):
+    form_class =forms.LogoutForm
+    template_name ='users/logout.html'
+
+    def form_valid(self,form):
+        logout(self.request)
+        return HttpResponseRedirect(reverse('home'))
+class SignUpView(CreateView):
+    form_class =UserCreationForm
+    template_name ='users/signup.html'
+    success_url = reverse_lazy('users:dashboard')
